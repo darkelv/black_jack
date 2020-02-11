e4ba6aea09a0c3b95ce022930ee956d17dcac524
darkelv <darkelv777@yandex.ru>
2020-02-11 15:07:13 +0300

    add_random_deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,14 +1,16 @@
+require_relative 'card'
+
 class Deck
   attr_accessor :cards
 
-  SUITS = [♠, ♥, ♣, ♦]
-  FACES = [2, 3, 4, 5, 6, 7,8, 9, 10, "В", "Д", "К", "Т"]
+  SUITS = ["♠", "♥", "♣", "♦"]
+  FACES = [2, 3, 4, 5, 6, 7, 8, 9, 10, "В", "Д", "К", "Т"]
 
   def initialize
-    @cards = create_deck
+    @cards = create_deck.shuffle!(random: Random.new(1))
   end
 
   def create_deck
-    SUITS.map { |suit| FACES.map { |face| Card.new(face, suit) } }
+    SUITS.map { |suit| FACES.map { |face| Card.new(face, suit) } }.flatten
   end
 end
